711a490a3791a67c37aee9aa3cedc098a5fdd287
Drewski Wiskus <andrewwiskus@gmail.com>
2018-10-13 14:17:00 -0500

    added full controller support, changed PlayerMovement to TwinStickMovement

--- SpritGam/Assets/Scripts/Controller/ControllerInput.cs
+++ SpritGam/Assets/Scripts/Controller/ControllerInput.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Key
+{
+    DOWN,
+    UP,
+    IS_PRESSED
+}
+
 // You can see the input mapping in the editor by going to
 // Edit -> Project-Settings -> Input
-public class ControllerInput : MonoBehaviour {
+public class ControllerInput : MonoBehaviour
+{
 
     public static float RightStickVertical()
     {
@@ -28,10 +36,78 @@ public class ControllerInput : MonoBehaviour {
 
     public static float RightTrigger()
     {
-        // this returns a float from 0 to -1. 
-        //  0 == not pressed
-        // -1 == fully pressed
+        return Input.GetAxis("Right_Trigger") * -1.0f; //reversing because it returns 0 to -1;
+    }
 
-        return Input.GetAxis("Right_Trigger");
+    public static float LeftTrigger()
+    {
+        return -Input.GetAxis("Left_Trigger");
+    }
+
+    private static bool ButtonPress(string for_button, Key key)
+    {
+        string joystick_button = "joystick button " + for_button;
+
+        if (key == Key.DOWN)
+        {
+            return Input.GetKeyDown(joystick_button);
+        }
+
+        if (key == Key.UP)
+        {
+            return Input.GetKeyUp(joystick_button);
+        }
+
+        return Input.GetKey(joystick_button);
+    }
+
+    public static bool Pressed_A(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("0", key);
+    }
+
+    public static bool Pressed_B(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("1", key);
+    }
+
+    public static bool Pressed_X(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("2", key);
+    }
+
+    public static bool Pressed_Y(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("3", key);
+    }
+
+    public static bool Pressed_LeftBumper(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("4", key);
+    }
+
+    public static bool Pressed_RightBumper(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("5", key);
+    }
+
+    public static bool Pressed_BackButton(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("6", key);
+    }
+
+    public static bool Pressed_StartButton(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("7", key);
+    }
+
+    public static bool Pressed_L3(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("8", key);
+    }
+
+    public static bool Pressed_R3(Key key = Key.IS_PRESSED)
+    {
+        return ControllerInput.ButtonPress("9", key);
     }
 }
---
+++ SpritGam/Assets/Scripts/PlayerActions/TwinStickMovement.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(AudioSource))]
+public class TwinStickMovement : MonoBehaviour
+{
+
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Transform m_player_transform;
+    [SerializeField] private float m_speed_multiplier;
+
+    private Rigidbody2D m_rigid_body;
+    private AudioSource m_audio_source;
+
+    void Awake()
+    {
+        m_rigid_body = GetComponent<Rigidbody2D>();
+        m_audio_source = GetComponent<AudioSource>();
+    }
+
+    void FixedUpdate()
+    {
+        m_rigid_body.velocity = new Vector2(Mathf.Lerp(0, ControllerInput.LeftStickHorizontal() * m_speed_multiplier, 0.8f),
+            Mathf.Lerp(0, ControllerInput.LeftStickVertical() * m_speed_multiplier, 0.8f));
+    }
+
+    void Update()
+    {
+        bool player_is_walking = ControllerInput.LeftStickHorizontal() != 0 || ControllerInput.LeftStickVertical() != 0;
+        bool player_is_aiming = ControllerInput.RightStickHorizontal() != 0 || ControllerInput.RightStickVertical() != 0;
+
+        if (player_is_aiming)
+        {
+            float angle = Controller.GetRightAnalogStickAngle();
+            m_player_transform.rotation = Quaternion.AngleAxis(angle, new Vector3(0, 0, 1));
+        }
+
+        if (player_is_walking)
+        {
+            if (m_audio_source.isPlaying == false)
+            {
+                m_audio_source.Play();
+            }
+
+            m_animator.Play("Walk");
+        }
+
+        if (player_is_aiming == false && player_is_walking == false)
+        {
+            m_animator.Play("Idle");
+        }
+    }
+
+}
