d748ff7677ba304f2403d8b22c030bb535263ec9
Drewski Wiskus <andrewwiskus@gmail.com>
2018-10-17 02:23:12 -0500

    added burst, auto burst, semi auto, and auto to gun controller

--- SpritGam/Assets/GunController.cs
+++ SpritGam/Assets/GunController.cs
@@ -7,58 +7,131 @@ public enum GunAnimationName
     TommyGun_Reload
 }
 
+public enum GunFireStyle
+{
+    AUTOMATIC,
+    SEMI_AUTOMATIC,
+    BURST_SEMIAUTO,
+    BURST_AUTOMATIC
+}
+
 public class GunController : MonoBehaviour
 {
     [SerializeField] private GameObject m_item_to_shoot;
     [SerializeField] private GameObject m_fire_point;
     [SerializeField] private WeaponAudio m_weapon_audio;
     [SerializeField] private Animator m_weapon_animator;
-    [SerializeField] private int m_clip_size;
-    [SerializeField] private float m_reload_time_in_seconds;
+    [SerializeField] private int m_clip_size = 60;
+    [SerializeField] private float m_reload_time_in_seconds = 1.75f;
     [SerializeField] private GunAnimationName m_reload_animation;
     [SerializeField] private GunGUIController m_gun_gui_controller;
-    [SerializeField] private float m_fire_rate_in_seconds;
+    [SerializeField] private float m_fire_rate_in_seconds = 0.15f;
     [SerializeField] private bool m_should_auto_reload = false;
+    [SerializeField] private GunFireStyle m_firerate_style;
+    [SerializeField] private float m_burst_speed = 0.05f;
+    [SerializeField] private int m_burst_count = 3;
+    [SerializeField] private float m_firerate_inbetween_bursts_in_seconds = 0.15f;
+
 
     private bool m_is_shooting_projectile = false;
     private int m_current_ammo = 0;
     private bool m_weapon_is_reloading = false;
+    private bool m_trigger_was_toggled = true;
 
     private void Awake()
     {
         m_current_ammo = m_clip_size;
         m_gun_gui_controller.SetClipStatus(m_current_ammo, m_clip_size);
+        StartCoroutine(start_trigger_listener());
     }
 
-    void Update()
+    private IEnumerator start_trigger_listener()
     {
 
-        if (ControllerInput.RightTrigger() != 0 && m_is_shooting_projectile == false && m_weapon_is_reloading == false)
-        {
 
-            Debug.Log("HOLDING DOWN TRIGGER");
-            m_is_shooting_projectile = true;
-            StartCoroutine(pull_gun_trigger());
+        while (ControllerInput.RightTrigger() <= 0.2)
+        {
+            yield return new WaitForEndOfFrame();
         }
 
-        if (ControllerInput.RightTrigger() == 0)
+
+        yield return pull_gun_trigger();
+
+        if (ControllerInput.RightTrigger() >= 0.2)
         {
-            m_is_shooting_projectile = false;
+            yield return new WaitForEndOfFrame();
         }
+        else
+        {
+            m_trigger_was_toggled = true;
+        }
+
+
+        yield return start_trigger_listener();
+        yield break;
     }
 
     private IEnumerator pull_gun_trigger()
     {
-        while (m_is_shooting_projectile == true)
+        if (ControllerInput.RightTrigger() <= 0.2)
         {
-            if(m_weapon_is_reloading == false)
+            yield break;
+        }
+
+
+        if (m_weapon_is_reloading == false)
+        {
+            switch (m_firerate_style)
             {
-                shoot_single_projectile();
-                yield return new WaitForSeconds(m_fire_rate_in_seconds);
-                if(m_weapon_is_reloading == true || m_is_shooting_projectile == false)
-                {
+
+                case GunFireStyle.AUTOMATIC:
+                    shoot_single_projectile();
+                    yield return new WaitForSeconds(m_fire_rate_in_seconds);
+                    yield return pull_gun_trigger();
+
                     yield break;
-                }
+
+                case GunFireStyle.BURST_SEMIAUTO:
+                    if (m_trigger_was_toggled)
+                    {
+
+                        for (int i = 0; i < m_burst_count; i++)
+                        {
+                            shoot_single_projectile();
+                            yield return new WaitForSeconds(m_burst_speed);
+                        }
+
+                        yield return new WaitForSeconds(m_firerate_inbetween_bursts_in_seconds);
+                        m_trigger_was_toggled = false;
+                    }
+
+                    break;
+
+                case GunFireStyle.BURST_AUTOMATIC:
+                    if (m_trigger_was_toggled)
+                    {
+
+                        for (int i = 0; i < m_burst_count; i++)
+                        {
+                            shoot_single_projectile();
+                            yield return new WaitForSeconds(m_burst_speed);
+                        }
+
+                        yield return new WaitForSeconds(m_firerate_inbetween_bursts_in_seconds);
+                       
+                        yield return pull_gun_trigger();
+                    }
+
+                    break;
+
+                case GunFireStyle.SEMI_AUTOMATIC:
+                    if (m_trigger_was_toggled)
+                    {
+                        shoot_single_projectile();
+                        m_trigger_was_toggled = false;
+                    }
+
+                    yield break;
             }
         }
 
@@ -67,7 +140,7 @@ public class GunController : MonoBehaviour
 
     public void ReloadWeapon()
     {
-        if(m_weapon_is_reloading == false)
+        if (m_weapon_is_reloading == false)
         {
             StartCoroutine(reload_weapon());
         }
@@ -97,7 +170,7 @@ public class GunController : MonoBehaviour
             m_weapon_audio.PlayFireGunSFX();
             m_current_ammo -= 1;
             m_gun_gui_controller.SetClipStatus(m_current_ammo, m_clip_size);
-            if(m_current_ammo == 0 && m_should_auto_reload)
+            if (m_current_ammo == 0 && m_should_auto_reload)
             {
                 StartCoroutine(reload_weapon());
             }
