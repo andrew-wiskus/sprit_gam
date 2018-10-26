eecba35a7db0ac428332719a0c8e8824c59532b8
Pat Hiniker <pathiniker@gmail.com>
2018-10-25 19:21:17 -0500

    added temporary cam follow method

---
+++ SpritGam/Assets/Scripts/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField] private GameObject player;
+    private Vector3 camPos;
+
+	// Use this for initialization
+	void Start () {
+        camPos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        camPos = player.transform.position;
+        transform.position = new Vector3 (camPos.x, camPos.y, -10);
+
+	}
+}
