5ff34953388f6d486fdefe47c1b80b2b75fd9ddb
Pat Hiniker <pathiniker@gmail.com>
2018-12-16 08:12:27 -0600

    ppu updates

---
+++ SpritGam/Assets/ResoultionController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResoultionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        // (width, height, true)
+        Screen.SetResolution(2000, 1200, true);
+        Debug.Log("set res");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
