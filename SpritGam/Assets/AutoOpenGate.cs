20f11facbcfc6c9ab41523e143ae88d8cba5a0d5
Andrew Wiskus <andrewwiskus@gmail.com>
2018-12-27 23:59:13 -0600

    added gate

---
+++ SpritGam/Assets/AutoOpenGate.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoOpenGate : MonoBehaviour {
+
+    // note, trigger is triggered on init??
+    private bool initial_lol = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(initial_lol)
+        {
+            GetComponent<Animator>().Play("GateOpen");
+
+        }
+
+        initial_lol = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        GetComponent<Animator>().Play("GateClose");
+    }
+}
