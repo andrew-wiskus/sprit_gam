d6a23daa8f2d2530f0481f727c4ab65a99cd6942
Pat Hiniker <pathiniker@gmail.com>
2019-01-27 01:08:16 -0600

    lets build a space ship

--- SpritGam/Assets/Scripts/Player/PlayerAim.cs
+++ SpritGam/Assets/Scripts/Player/PlayerAim.cs
@@ -8,6 +8,7 @@ public class PlayerAim : MonoBehaviour
     public float Angle { get { return m_player_aim_angle; } }
     public float Magnitude { get { return m_aim_magnitude; } }
 
+    private float defined_direction;
     [SerializeField] private float m_right_stick_dead_zone = 0.2f;
 
     private bool m_player_is_aiming;
@@ -16,9 +17,10 @@ public class PlayerAim : MonoBehaviour
 
     private void FixedUpdate()
     {
-        m_player_aim_angle = get_adjusted_aim_angle();
+        
         m_aim_magnitude = aim_magnitude();
         m_player_is_aiming = m_aim_magnitude > m_right_stick_dead_zone;
+        m_player_aim_angle = get_adjusted_aim_angle();
     }
 
     private float aim_magnitude()
@@ -31,8 +33,24 @@ public class PlayerAim : MonoBehaviour
     private float get_adjusted_aim_angle()
     {
         float angle = Controller.GetRightAnalogStickAngle();
-//
+        //
+        //Debug.Log("INIT X: " + angle);
+
         float x = angle - 360.0f;
-        return x >= 0.0f ? x : x * -1.0f;
+
+        //Debug.Log("X ANGLE: " + x);
+
+
+        if (Magnitude < m_right_stick_dead_zone)
+        {
+            return defined_direction;
+        } else
+        {
+            defined_direction = x * -1.0f;
+            return defined_direction;
+        }
+
+        //return x >= 0.0f ? x : x * -1.0f;
+        
     }
 }
